60813fdad3c9eb16b4662fac939c6359f9d5dab6
Crowdbotics <team@crowdbotics.com>
2020-11-19 07:35:38 +0000

    Update models

--- backend/taco_22766/wsgi.py
+++ backend/taco_22766/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taco_22766.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "taco_22766.settings")
 
 application = get_wsgi_application()
